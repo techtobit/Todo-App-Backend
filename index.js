bffb7dbc56030676ced316be2b61e27a56dc4dc4
unknown <nurthern2016@gmail.com>
2022-07-01 04:23:26 +0600

    Shared Model , TaskFinish Col ,Delete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors');
 require("dotenv").config();
@@ -18,8 +18,11 @@ async function run() {
  try {
   await client.connect();
   const taskCollection = client.db("TodoApp").collection("Task");
-  // const taskFinishCollection = client.db("TodoApp").collection("TaskFinish");
+  const taskFinishCollection = client.db("TodoApp").collection("TaskFinish");
 
+  // ---------------- Todo App ---------------//
+
+  //Display All Task
   app.get('/task', async (req, res) => {
    const query = {};
    const cursor = taskCollection.find(query)
@@ -27,12 +30,38 @@ async function run() {
    res.send(tasks);
   })
 
+  //For Editing & Updating Task
+  app.get('/task/:id', async (req, res) => {
+   const id = req.params.id;
+   const query = {_id : ObjectId(id)};
+   const cursor = taskCollection.find(query)
+   const tasks = await cursor.toArray()
+   res.send(tasks);
+  })
 
 
+  //For Add new Task on list
   app.post('/task', async (req, res) => {
    const data = req.body;
    const newTask = await taskCollection.insertOne(data)
    res.send(newTask);
+  })
+
+
+  //Once Task Finish it will Save on new Database to show complied
+  app.post('/taskFinish', async (req, res) => {
+   const data = req.body;
+   const newTaskFinish = await taskFinishCollection.insertOne(data)
+   res.send(newTaskFinish);
+  })
+
+
+  //The Finish task will remove from List
+  app.delete('/task/:id', async (req, res) => {
+   const id = req.params.id;
+   const query = { _id: ObjectId(id) };
+   const taskFinish = taskCollection.deleteOne(query)
+   res.send(taskFinish);
   })
 
 
